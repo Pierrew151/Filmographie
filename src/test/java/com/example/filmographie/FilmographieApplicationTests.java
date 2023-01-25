3de196b19906e973f753483c5cc85641a5fa7e90
Pierrew151 <p.weber145@laposte.net>
2023-01-25 09:42:09 +0100

    init

---
+++ src/test/java/com/example/filmographie/FilmographieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmographie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmographieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
